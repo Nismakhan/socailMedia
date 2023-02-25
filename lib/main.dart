658a3a69757ab0e8b52114a479bdda207ecc34ff
wajahatiqbal22 <wajahatiqbal22@gmail.com>
2023-02-25 13:53:13 +0500

    followed, followers, notifications, fcm

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:social_media_app/app/social_media.dart';
 
@@ -7,5 +10,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   // await FirebaseAuth.instance.signOut();
+
   return runApp(const SocialMedia());
 }
